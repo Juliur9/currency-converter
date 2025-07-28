838b14c265672c263f471d4cea258748cab9e659
Juliur9 <url.julian@gmail.com>
2025-07-28 16:05:02 +0200

    Cool Design for converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { CurrencyOptions } from './CurrencyOptions'
+
 function App() {
   return (
     <>
-      <p className="text-slate-800">Welcome to the React application!</p>
+      <div className="mt-10 flex flex-col items-center bg-gray-50">
+        <div className="flex flex-col items-center rounded-lg bg-slate-100 p-6 shadow-lg">
+          <div className="mb-5 flex items-center justify-center gap-4">
+            <div className="flex w-[510px] items-center justify-between rounded-lg border-2 border-gray-300 bg-gray-100 p-4 shadow-md">
+              <label for="currencies" className="mr-4 text-lg">
+                From Currency:
+              </label>
+              <CurrencyOptions />
+            </div>
+            <input
+              type="number"
+              className="h-8 w-36 rounded-lg bg-gray-200 p-2"
+            />
+          </div>
+
+          <div className="mb-5 flex items-center justify-center gap-4">
+            <div className="flex w-[510px] items-center justify-between rounded-lg border-2 border-gray-300 bg-gray-100 p-4 shadow-md">
+              <label for="currencies" className="mr-4 text-lg">
+                To Currency:
+              </label>
+              <CurrencyOptions />
+            </div>
+            <input
+              type="number"
+              disabled
+              className="h-8 w-36 rounded-lg bg-gray-200 p-2"
+            />
+          </div>
+          <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+            Convert
+          </button>
+        </div>
+      </div>
     </>
   )
 }
